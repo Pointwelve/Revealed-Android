1eab84b74f9c0ff646b32ca279553e789d01d71a
Alvin Choo <kokhong200@gmail.com>
2020-02-09 23:18:26 +0800

    Init Auth 0 Android

--- app/src/main/java/com/pointwelve/revealed/ui/main/MainViewModel.kt
+++ app/src/main/java/com/pointwelve/revealed/ui/main/MainViewModel.kt
@@ -1,7 +1,42 @@
 package com.pointwelve.revealed.ui.main
 
+import android.app.Activity
+import android.app.Dialog
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.provider.AuthCallback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(private val account: Auth0) : ViewModel() {
+    val authLiveData = MutableLiveData<Boolean>()
+
+    fun login(activity: Activity) = viewModelScope.launch {
+        //Declare the callback that will receive the result
+        val authCallback = object : AuthCallback {
+            override fun onFailure(dialog: Dialog) {
+                //failed with a dialog
+                dialog.show()
+            }
+
+            override fun onFailure(exception: AuthenticationException) {
+                //failed with an exception
+                Timber.e(exception)
+            }
+
+            override fun onSuccess(credentials: Credentials) {
+                //succeeded!
+                authLiveData.value = true
+            }
+        }
+
+        WebAuthProvider.login(account)
+            .start(activity, authCallback)
+    }
 }
