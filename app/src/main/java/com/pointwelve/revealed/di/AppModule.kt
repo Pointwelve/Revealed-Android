d53684fbb9ea8853e7ced56c75471017d62c18c9
Alvin Choo <kokhong200@gmail.com>
2020-02-13 22:57:42 +0800

    feat: Exposed apollo client. Generated models from schema

--- app/src/main/java/com/pointwelve/revealed/di/AppModule.kt
+++ app/src/main/java/com/pointwelve/revealed/di/AppModule.kt
@@ -1,19 +1,59 @@
 package com.pointwelve.revealed.di
 
 import android.app.Application
+import com.apollographql.apollo.ApolloClient
 import com.auth0.android.Auth0
+import com.pointwelve.revealed.BuildConfig
 import com.pointwelve.revealed.R
 import dagger.Module
 import dagger.Provides
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module(includes = [ViewModelModule::class])
 class AppModule {
+    private companion object {
+        const val CONNECT_TIMEOUT = 10L
+        const val WRITE_TIMEOUT = 10L
+        const val READ_TIMEOUT = 10L
+        const val CACHE_SIZE = 10 * 1024 * 1024 // 10MB
+    }
+
     @Singleton
     @Provides
     fun provideAuth0(app: Application): Auth0 {
         val account = Auth0(app)
         account.isOIDCConformant = true
         return account
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttp(app: Application): OkHttpClient {
+        val client = OkHttpClient.Builder()
+            .cache(Cache(app.cacheDir, CACHE_SIZE.toLong()))
+            .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            .addInterceptor(
+                HttpLoggingInterceptor().setLevel(
+                    if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE)
+            )
+
+        return client.build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApolloClient(app: Application, okHttpClient: OkHttpClient): ApolloClient {
+        val apolloClient = ApolloClient.builder()
+            .serverUrl(app.getString(R.string.graphql_host))
+            .okHttpClient(okHttpClient)
+            .build()
+
+        return apolloClient
     }
 }
